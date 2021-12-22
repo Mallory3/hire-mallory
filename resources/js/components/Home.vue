7d6661f854f5c6db075a76f60084e4de2377b767
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2021-12-21 20:43:13 -0700

    Correcting custom styles

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -423,13 +423,37 @@ import VueScrollSnap from "vue-scroll-snap";
 </script>
 
 <style>
-.item {
-  /* Set the minimum height of the items to be the same as the height of the scroll-snap-container.*/
-  min-height: 100vh;
-}
+/* FONTS */
+  @import url('https://fonts.googleapis.com/css2?family=Geo&family=Open+Sans:wght@800&display=swap');
 
-.scroll-snap-container {
-  height: 100vh;
-  width: 100vw;
-}
+  .geo {
+    font-family: 'Geo', sans-serif;
+  }
+
+  .text-header {
+    font-family: 'Open Sans', sans-serif;
+  }
+  /* THEME */
+  .router-link-exact-active {
+    color: rgb(96,175,65);
+  }
+  .button-green {
+    background-color: rgb(96,175,65) !important;
+  }
+  :hover.button-green {
+    background-image: linear-gradient(to right, rgb(96,175,65), rgb(86, 206, 38));
+  }
+  .text-coral {
+      color: rgb(241,82,46);
+  }
+  .bg-coral {
+      background-color: rgb(241,82,46);
+  }
+  .bg-coral-grad {
+    background-color: rgb(241,82,46);
+    background: linear-gradient(90deg, rgba(241,82,46,1) 0%, rgba(250,48,0,1) 100%);
+  }
+  .bg-teal {
+    background-color: rgb(29,72,79);
+  }
 </style>
